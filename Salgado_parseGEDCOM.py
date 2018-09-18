ed24a772e51be5e13c4c81dabb46ec9b3a2fe871
Bruno-LocalPC <bsalgado@stevens.edu>
2018-09-17 22:33:27 -0400

    changes

--- Salgado_parseGEDCOM.py
+++ Salgado_parseGEDCOM.py
@@ -1,5 +1,5 @@
 supportedTags = {"INDI":0, "NAME":1, "SEX":1, "BIRT":1, "DEAT":1, "FAMC":1, "FAMS":1, "FAM":0, "MARR":1, "HUSB":1, "WIFE":1, "CHIL":1, "DIV":1, "DATE":2, "HEAD":0, "TRLR":0, "NOTE":0}
-gedcomFile = open("proj02test.ged", "r")
+gedcomFile = open("familyTree.ged", "r")
 
 treeList = {}
 individualList = {}
