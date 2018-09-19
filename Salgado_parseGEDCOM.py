c6c2c51d4a91a3dbc579288270758aada4ebb7a7
Dan Barry <dbarry@stevens.edu>
2018-09-19 17:26:54 -0400

    Clean up more code

--- Salgado_parseGEDCOM.py
+++ Salgado_parseGEDCOM.py
@@ -9,7 +9,7 @@ treeList = {}
 individualList = {}
 
 
-def addtag(isIndi, id, tag, value):
+def addTag(isIndi, id, tag, value):
     if value is not "":
         if isIndi:
             db = individualList
@@ -50,9 +50,9 @@ def parse(gedcomFile):
                     isIndi = False
         elif fileLines[1] in supportedTags.keys() and level in supportedTags.values():
             if fileLines[1] == "DATE":
-                addtag(isIndi, zeroLevelId, prevTag, arguments)
+                addTag(isIndi, zeroLevelId, prevTag, arguments)
             else:
-                addtag(isIndi, zeroLevelId, fileLines[1], arguments)
+                addTag(isIndi, zeroLevelId, fileLines[1], arguments)
             if supportedTags[fileLines[1]] == level:
                 valid = "Y"
 
@@ -61,49 +61,39 @@ def parse(gedcomFile):
 
 def printTree():
     indiTable = PrettyTable(["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"])
+    columns = ["NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS"]
     for id, args in individualList.items():
-        if "NAME" not in args.keys():
-            args["NAME"] = "NA"
-        if "SEX" not in args.keys():
-            args["SEX"] = "NA"
-        if "BIRT" not in args.keys():
-            args["BIRT"] = "NA"
-            birth = datetime.date(1, 1, 1)  # just to prevent errors
-        else:
+        for col in columns:
+            if col not in args.keys():
+                args[col] = "NA"
+        if args["BIRT"] is not "NA":
             birth = datetime.datetime.strptime(args["BIRT"], "%d %b %Y").date()
-        if "DEAT" not in args.keys():
-            args["DEAT"] = "NA"
-            age = datetime.date.today() - birth
         else:
+            birth = datetime.date(1, 1, 1)  # just to prevent errors
+        if args["DEAT"] is not "NA":
             death = datetime.datetime.strptime(args["DEAT"], "%d %b %Y").date()
             age = death - birth
-        if "FAMC" not in args.keys():
-            args["FAMC"] = "NA"
-        if "FAMS" not in args.keys():
-            args["FAMS"] = "NA"
+        else:
+            age = datetime.date.today() - birth
         indiTable.add_row(
             [id, args["NAME"], args["SEX"], args["BIRT"], age.days // 365, args["DEAT"] == "NA", args["DEAT"],
              args["FAMC"], args["FAMS"]])
 
     famTable = PrettyTable(
         ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"])
+    columns = ["MARR", "DIV", "HUSB", "WIFE", "CHIL"]
     for id, args in treeList.items():
-        if "MARR" not in args.keys():
-            args["MARR"] = "NA"
-        if "DIV" not in args.keys():
-            args["DIV"] = "NA"
-        if "HUSB" not in args.keys():
-            args["HUSB"] = "NA"
-            husbName = "NA"
-        else:
+        for col in columns:
+            if col not in args.keys():
+                args[col] = "NA"
+        if args["HUSB"] is not "NA":
             husbName = individualList[args["HUSB"]]["NAME"]
-        if "WIFE" not in args.keys():
-            args["WIFE"] = "NA"
-            wifeName = "NA"
         else:
+            husbName = "NA"
+        if args["WIFE"] is not "NA":
             wifeName = individualList[args["WIFE"]]["NAME"]
-        if "CHIL" not in args.keys():
-            args["CHIL"] = "NA"
+        else:
+            wifeName = "NA"
         famTable.add_row([id, args["MARR"], args["DIV"], args["HUSB"], husbName, args["WIFE"], wifeName, args["CHIL"]])
 
     print("Individuals")
