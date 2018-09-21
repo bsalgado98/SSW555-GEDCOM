b44578209d881fd1cb00ae8baf90a09b333cd09a
Bruno-LocalPC <bsalgado@stevens.edu>
2018-09-21 17:14:30 -0400

    misc changes

--- Salgado_parseGEDCOM.py
+++ Salgado_parseGEDCOM.py
@@ -1,6 +1,7 @@
 import datetime
 import sys
 from prettytable import PrettyTable
+import cmd
 
 supportedTags = {"INDI": 0, "NAME": 1, "SEX": 1, "BIRT": 1, "DEAT": 1, "FAMC": 1, "FAMS": 1, "FAM": 0, "MARR": 1,
                  "HUSB": 1, "WIFE": 1, "CHIL": 1, "DIV": 1, "DATE": 2, "HEAD": 0, "TRLR": 0, "NOTE": 0}
@@ -123,7 +124,6 @@ def printtree():
     print(indiTable.get_string(sortby="ID"))
     print("Families")
     print(famTable.get_string(sortby="ID"))
-
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as file:
