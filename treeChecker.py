021620f44386ac90102a0dd7f2b98ea927743b13
Bruno-LocalPC <bsalgado@stevens.edu>
2018-09-21 18:43:05 -0400

    birthBeforeDeath

--- treeChecker.py
+++ treeChecker.py
@@ -8,12 +8,27 @@ supportedTags = {"INDI": 0, "NAME": 1, "SEX": 1, "BIRT": 1, "DEAT": 1, "FAMC": 1
                  "HUSB": 1, "WIFE": 1, "CHIL": 1, "DIV": 1, "DATE": 2, "HEAD": 0, "TRLR": 0, "NOTE": 0}
 
 def birthBeforeMarriage(treeList, individualList):
+    individualBirthdays = {}
+    individualMarriages = {}
     for key, value in individualList.items():
-        if key.contains("I"):
-            print (value.get("NAME"))
-        
+        if "I" in key:
+            individualBirthdays[value.get("NAME")] = value.get("BIRT")
+        if "F" in key:
+            individualMarriages[value.get("HUSB"), value.get("WIFE")] = value.get("MARR")
+    print(individualBirthdays)    
+    print(individualMarriages)
+    
+def birthBeforeDeath(treeList, individualList):
+    individualBirthdays = {}
+    individualDeaths = {}
+    for key, value in individualList.items():
+        if "I" in key:
+            individualBirthdays[value.get("NAME")] = value.get("BIRT")
+    
 def us06(treeList, individualList):
     pass
 
 def main(treeList, individualList):
     us06(treeList, individualList)
+    #birthBeforeMarriage(treeList, individualList)
+    birthBeforeDeath(treeList, individualList)
