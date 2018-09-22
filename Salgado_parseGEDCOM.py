ae958fadd37d05a939cc81bc41f1338999d9209e
Dan Barry <dbarry@stevens.edu>
2018-09-22 00:53:30 -0400

    Add new base dict for individuals and families

--- Salgado_parseGEDCOM.py
+++ Salgado_parseGEDCOM.py
@@ -1,4 +1,3 @@
-import cmd
 supportedTags = {"INDI": 0, "NAME": 1, "SEX": 1, "BIRT": 1, "DEAT": 1, "FAMC": 1, "FAMS": 1, "FAM": 0, "MARR": 1,
                  "HUSB": 1, "WIFE": 1, "CHIL": 1, "DIV": 1, "DATE": 2, "HEAD": 0, "TRLR": 0, "NOTE": 0}
 
@@ -12,7 +11,7 @@ def addTag(isIndi, id, tag, value):
             db = individualList
         else:
             db = treeList
-        if tag not in db[id].keys() or db[id][tag] == "":
+        if db[id][tag] == "NA" or db[id][tag] == "":
             db[id][tag] = value
         else:
             if isinstance(db[id][tag], list):
@@ -28,7 +27,6 @@ def parse(gedcomFile):
         fileLines = line.split()
         level = int(fileLines[0])
         tag = fileLines[1]
-        valid = "N"
         arguments = " ".join(fileLines[2:])
 
         if fileLines[1] == "NOTE":
@@ -36,38 +34,27 @@ def parse(gedcomFile):
         if "INDI" in fileLines or "FAM" in fileLines:
             if level == 0 and (fileLines[2] == "INDI" or fileLines[2] == "FAM"):
                 zeroLevelId = fileLines[1]
-                valid = "Y"
                 tag = fileLines[2]
                 arguments = fileLines[1]
                 if fileLines[2] == "INDI":
-                    individualList[arguments] = {}
+                    individualList[arguments] = {"NAME": "NA",
+                                                 "SEX": "NA",
+                                                 "BIRT": "NA",
+                                                 "DEAT": "NA",
+                                                 "FAMC": "NA",
+                                                 "FAMS": "NA"}
                     isIndi = True
                 else:
-                    treeList[arguments] = {}
+                    treeList[arguments] = {"MARR": "NA",
+                                           "HUSB": "NA",
+                                           "WIFE": "NA",
+                                           "CHIL": "NA",
+                                           "DIV": "NA"}
                     isIndi = False
         elif fileLines[1] in supportedTags.keys() and level in supportedTags.values():
             if fileLines[1] == "DATE":
                 addTag(isIndi, zeroLevelId, prevTag, arguments)
             else:
                 addTag(isIndi, zeroLevelId, fileLines[1], arguments)
-            if supportedTags[fileLines[1]] == level:
-                valid = "Y"
-
         prevTag = tag
     return treeList, individualList
-
-
-
-
-# # if __name__ == "__main__":
-#     with open(sys.argv[1], "r") as file:
-#         gedcomFile = file.readlines()
-#     parse(gedcomFile)
-#     printTree()
-#
-# # for testing within IDE
-# with open("familyTree.ged", "r") as file:
-#     gedcomFile = file.readlines()
-# parse(gedcomFile)
-# printTree()
-# treeChecker
--- treeChecker.py
+++ treeChecker.py
@@ -7,13 +7,16 @@ import Salgado_parseGEDCOM
 supportedTags = {"INDI": 0, "NAME": 1, "SEX": 1, "BIRT": 1, "DEAT": 1, "FAMC": 1, "FAMS": 1, "FAM": 0, "MARR": 1,
                  "HUSB": 1, "WIFE": 1, "CHIL": 1, "DIV": 1, "DATE": 2, "HEAD": 0, "TRLR": 0, "NOTE": 0}
 
+
 def birthBeforeMarriage(treeList, individualList):
     for key, value in individualList.items():
         if key.contains("I"):
-            print (value.get("NAME"))
-        
+            print(value.get("NAME"))
+
+
 def us06(treeList, individualList):
     pass
+
 
 def main(treeList, individualList):
     us06(treeList, individualList)
