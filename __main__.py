450d5032181d0a10936992bd2317e6fcc6afd1b2
Bruno-LocalPC <bsalgado@stevens.edu>
2018-10-02 21:59:48 -0400

    Final updates for project 04

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ import Salgado_parseGEDCOM
 import treeChecker
 import printTable
 
-with open("familyTree.ged", "r") as file:
+with open("invalidFamilyTree.ged", "r") as file:
     gedcomFile = file.readlines()
 # Remember to comment out above `with` statement and uncomment below `with` statement when submitting
 # with open(sys.argv[1], "r") as file:
